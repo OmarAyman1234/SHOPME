8f4d05222ab0b0aeef477a8e9bac2c9e4d159bb9
OmarAyman1234 <167465513+OmarAyman1234@users.noreply.github.com>
2024-09-09 00:56:07 +0300

    Changed images path to work

--- data/data.js
+++ data/data.js
@@ -32,22 +32,22 @@ export const categories = [
   {
     id: '1',
     name: 'Appliances',
-    image: '../Images/appliances/1_appliances-group.png',
+    image: './Images/appliances/1_appliances-group.png',
   },
   {
     id: '2',
     name: 'Kitchenware',
-    image: '../Images/kitchenware/1_kitchenware-display3.png',
+    image: './Images/kitchenware/1_kitchenware-display3.png',
   }, 
   {
     id: '3',
     name: 'Clothing',
-    image:'../Images/clothing/1_shirts-display.jpg',
+    image:'./Images/clothing/1_shirts-display.jpg',
   },
   {
     id: '4',
     name: 'House Components',
-    image: '../Images/house-components/1_house-components.jpg',
+    image: './Images/house-components/1_house-components.jpg',
     //Chairs, beds ... etc
   }
 ];
@@ -58,7 +58,7 @@ const products = [
     type: 'appliances',
     id: '1000', 
     name: 'Espresso Machine', 
-    image: '../Images/appliances/espresso-machine-two-cups.png', 
+    image: './Images/appliances/espresso-machine-two-cups.png', 
     price: 2000,
     description: 'Turn up the heat and make two coffee cups at the same time!!'
   },
@@ -66,7 +66,7 @@ const products = [
     type: 'appliances',
     id: '1001', 
     name: 'Refrigerator', 
-    image: '../Images/appliances/refrigerator.png', 
+    image: './Images/appliances/refrigerator.png', 
     price: 30000,
     description: ''
   },
@@ -74,7 +74,7 @@ const products = [
     type: 'appliances',
     id: '1002', 
     name: 'Microwave Oven', 
-    image: '../Images/appliances/microwave-oven.png', 
+    image: './Images/appliances/microwave-oven.png', 
     price: 15000,
     description: ''
   },
@@ -82,7 +82,7 @@ const products = [
     type: 'appliances',
     id: '1003', 
     name: 'Iron Appliance', 
-    image: '../Images/appliances/iron-appliance.png', 
+    image: './Images/appliances/iron-appliance.png', 
     price: 2000,
     description: ''
   },
@@ -92,7 +92,7 @@ const products = [
     type: 'kitchenware',
     id: '1004',
     name: 'Gas Cooker With Oven',
-    image: '../Images/kitchenware/gas-cooker-with-oven.png',
+    image: './Images/kitchenware/gas-cooker-with-oven.png',
     price: 25000,
     description: ''
   },
@@ -100,7 +100,7 @@ const products = [
     type: 'kitchenware',
     id: '1005',
     name: 'Hand Mixer',
-    image: '../Images/kitchenware/hand-mixer.png',
+    image: './Images/kitchenware/hand-mixer.png',
     price: 800,
     description: ''
   }, 
@@ -108,7 +108,7 @@ const products = [
     type: 'kitchenware',
     id: '1006',
     name: 'Blue Cup',
-    image: '../Images/kitchenware/blue-cup.png',
+    image: './Images/kitchenware/blue-cup.png',
     price: 50,
     description: ''
   },
@@ -116,7 +116,7 @@ const products = [
     type: 'kitchenware',
     id: '1007',
     name: 'Cutting Board',
-    image: '../Images/kitchenware/cutting-board-wooden.png',
+    image: './Images/kitchenware/cutting-board-wooden.png',
     price: 200,
     description: 'A wooden cutting board'
   },
@@ -124,7 +124,7 @@ const products = [
     type: 'kitchenware',
     id: '1008',
     name: 'Fork',
-    image: '../Images/kitchenware/fork-kitchenware.jpg',
+    image: './Images/kitchenware/fork-kitchenware.jpg',
     price: 70,
     description: 'A stainless steel fork'
   },
@@ -132,7 +132,7 @@ const products = [
     type: 'kitchenware',
     id: '1009',
     name: 'Frying Pan',
-    image: '../Images/kitchenware/frying-pan.png',
+    image: './Images/kitchenware/frying-pan.png',
     price: 400,
     description: ''
   },
@@ -140,7 +140,7 @@ const products = [
     type: 'kitchenware',
     id: '1010',
     name: 'Glass Cup',
-    image: '../Images/kitchenware/glass-cup.png',
+    image: './Images/kitchenware/glass-cup.png',
     price: 75,
     description: ''
   },
@@ -148,7 +148,7 @@ const products = [
     type: 'kitchenware',
     id: '1011',
     name: 'Soup Ladle',
-    image: '../Images/kitchenware/plastic-soup-ladle.png',
+    image: './Images/kitchenware/plastic-soup-ladle.png',
     price: 60,
     description: 'A plastic soup ladle'
   },
@@ -156,7 +156,7 @@ const products = [
     type: 'kitchenware',
     id: '1012',
     name: 'Rolling Pin',
-    image: '../Images/kitchenware/rolling-pin-wooden.png',
+    image: './Images/kitchenware/rolling-pin-wooden.png',
     price: 55,
     description: 'A wooden rolling pin'
   },
@@ -164,7 +164,7 @@ const products = [
     type: 'kitchenware',
     id: '1013',
     name: 'Dishware Set',
-    image: '../Images/kitchenware/set-of-dishware-stainless-steel.png',
+    image: './Images/kitchenware/set-of-dishware-stainless-steel.png',
     price: 900,
     description: 'A set of stainless steel dishware'
   }, 
@@ -172,7 +172,7 @@ const products = [
     type: 'kitchenware',
     id: '1014',
     name: 'Cooking Spoon',
-    image: '../Images/kitchenware/spoon-wooden.png',
+    image: './Images/kitchenware/spoon-wooden.png',
     price: 95,
     description: 'A wooden spoon for cooking'
   },
@@ -180,7 +180,7 @@ const products = [
     type: 'kitchenware',
     id: '1015',
     name: 'Cooking Spoons Set',
-    image: '../Images/kitchenware/spoons-wooden.png',
+    image: './Images/kitchenware/spoons-wooden.png',
     price: 250,
     description: 'A set of wooden spoons for cooking'
   },
@@ -188,7 +188,7 @@ const products = [
     type: 'kitchenware',
     id: '1016',
     name: 'Square Grill Pan',
-    image: '../Images/kitchenware/square-grill-pan.png',
+    image: './Images/kitchenware/square-grill-pan.png',
     price: 340,
     description: ''
   },
@@ -196,7 +196,7 @@ const products = [
     type: 'kitchenware',
     id: '1017',
     name: 'Teapot',
-    image: '../Images/kitchenware/teapot-red.png',
+    image: './Images/kitchenware/teapot-red.png',
     price: 180,
     description: 'A red teapot'
   },
@@ -204,7 +204,7 @@ const products = [
     type: 'clothing',
     id: '1018',
     name: 'T-Shirt',
-    image: '../Images/clothing/men-tshirt-black.png',
+    image: './Images/clothing/men-tshirt-black.png',
     price: 400,
     description: 'A black tshirt for men'
   },
@@ -212,7 +212,7 @@ const products = [
     type: 'clothing',
     id: '1019',
     name: 'Belt',
-    image: '../Images/clothing/leather-belt-black.png',
+    image: './Images/clothing/leather-belt-black.png',
     price: 200,
     description: 'A black leather belt for men'
   }
